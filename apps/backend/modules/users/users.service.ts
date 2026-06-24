da4217f8487513d4c08e0588e8eda5a93091db39
Jamal Amoudi <jamaldamdoudi@gmail.com>
2026-06-24 21:30:51 +0300

    feat: add user module

---
+++ apps/backend/modules/users/users.service.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from "./users.repository";
+import { User } from "./users.schema";
+
+class UserService {
+    private repository = new UserRepository();
+
+    getUsers(page: number, limit: number): Promise<User[]> {
+        return this.repository.findAll({});
+    }
+
+    getUser(id: string): Promise<User | null> {
+        return this.repository.findById(id);
+    }
+
+    public findByEmail(email: string): Promise<User | null> {
+        return this.repository.findByEmail(email);
+    }
+
+    public createUser(name: string, email: string, password: string, avatar?: string): Promise<User> {
+        return this.repository.create(name, email, password, avatar);
+    }
+
+    updateUser(id: string, name?: string, email?: string, avatar?: string): Promise<User> {
+        return this.repository.update(id, name, email, avatar);
+    }
+
+    deleteUser(id: string): Promise<boolean> {
+        return this.repository.delete(id);
+    }
+
+    isUserIdExist(id: string): Promise<boolean> {
+        return this.repository.findById(id).then(user => !!user);
+    }
+}
+
+export const userService = new UserService(); 
