afc9647c3c45ef33baae469b6f644933333cff35
JamalAmoudi <jamaldamoudi@gmail.com>
2026-07-05 17:26:57 +0300

    refactor: improve app and server initialization

--- apps/backend/app.ts
+++ apps/backend/app.ts
@@ -1,17 +1,33 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
-// import authRouter from './modules/auth/auth.router'
-// import usersRouter from './modules/users/users.router'
+import { resumeRouter } from './modules/resume/resume.routes'
+import { authRouter } from './modules/auth/auth.routes'
+import { userRouter } from './modules/users/users.routes'
+import { templateRouter } from './modules/template/template.routes'
+import { getEnvOrThrow } from './common/utils/util'
+import session from 'express-session'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
+app.use(express.urlencoded());
+app.use(
+    session({
+        secret: getEnvOrThrow('SESSION_SECRET'),
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: true, maxAge: 1000 * 60 * 60 * 24 * 30 }
+    })
+);
+
 
 // Routes
-// app.use('/api/auth', authRouter)
-// app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
+app.use('/api/templates', templateRouter)
 
 export default app
--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -1,7 +1,12 @@
 import app from './app';
+import { getEnvOrThrow } from './common/utils/util';
 
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = getEnvOrThrow('PORT');
+
+if (process.env.NODE_ENV !== "test") {
+    app.listen(3001, () => {
+        console.log(`App is running on PORT ${PORT}`);
+    })
+};
+
