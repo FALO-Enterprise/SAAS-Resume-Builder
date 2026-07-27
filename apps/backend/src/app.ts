60a241fae41b5f5146b6b5324a5900f864f42877
hashem-mdoukh <hashmmdwkh@gmail.com>
2026-07-27 17:16:42 +0300

    feafactor(app/backend): refactor cros in app.ts file in back end server

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -13,7 +13,10 @@ const app = express()
 
 app.use(express.json())
 app.use(responseEnhancer);
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
 app.use(helmet())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded());
